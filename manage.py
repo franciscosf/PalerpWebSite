be3a44155b79c550ff02b2c0ff1d2bba5bb7a03a
franciscosf <161133@unsaac.edu.pe>
2018-04-02 09:25:58 -0500

    Some changes 1

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Web1.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PalerpWebSite.settings")
 
     from django.core.management import execute_from_command_line
 
